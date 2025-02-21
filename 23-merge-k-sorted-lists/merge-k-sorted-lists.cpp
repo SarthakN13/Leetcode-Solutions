d828d42f2c445d4e8a3a2003f56aa7fbef964a8e
Sarthak Nahar <163443733+SarthakN13@users.noreply.github.com>
2025-02-21 15:23:16 +0530

    Time: 99 ms (16.22%) | Memory: 18.9 MB (27.37%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,49 @@
+/**
+
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+ListNode* merge(ListNode* a, ListNode* b) {
+        ListNode dummy(0);         // Create a dummy node
+        ListNode* tail = &dummy;     // Tail pointer to build the merged list
+
+        // Loop until one of the lists is empty
+        while (a != nullptr && b != nullptr) {
+            if (a->val <= b->val) {
+                tail->next = a;    // Link the smaller node
+                a = a->next;       // Move pointer forward in list a
+            } else {
+                tail->next = b;    // Link the smaller node
+                b = b->next;       // Move pointer forward in list b
+            }
+            tail = tail->next;     // Move the tail pointer forward
+        }
+
+        // Attach the remaining part of the list that is not empty
+        tail->next = (a != nullptr) ? a : b;
+
+        return dummy.next; // The merged list starts after the dummy node
+    }
+
+
+    ListNode* mergeKLists(vector<ListNode*>& arr) {
+        if(arr.size()==0) return NULL;
+        while(arr.size()>1){
+            ListNode * a = arr[arr.size()-1];
+            arr.pop_back();
+            ListNode * b = arr[arr.size()-1];
+            arr.pop_back();
+            ListNode *c = merge(a,b);
+            arr.push_back(c);
+        }
+        return arr[0];
+    }
+};
